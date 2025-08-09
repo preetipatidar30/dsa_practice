96089541568e3e132820ddf62ac53df08ac01a53
preeti patidar <162000314+preetipatidar30@users.noreply.github.com>
2025-08-09 22:03:09 +0530

    Time: 0 ms (100%), Space: 63.4 MB (78.28%) - LeetHub

---
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int n=arr.size();
+        for(int i=0;i<n-1;i++){
+            if(arr[i]>arr[i+1]){
+                return i;
+                break;
+            }
+        }
+        return -1;
+    }
+};
