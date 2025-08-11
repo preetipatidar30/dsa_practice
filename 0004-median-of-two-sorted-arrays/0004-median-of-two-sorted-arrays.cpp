97113565239b9f5c598ad3e68e5f052acc0372c8
preeti patidar <162000314+preetipatidar30@users.noreply.github.com>
2025-08-11 14:46:04 +0530

    Time: 155 ms (5.02%), Space: 95.2 MB (60.61%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -3,33 +3,34 @@ public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
        int n=nums1.size();
        int m=nums2.size();
-       vector<int> ans;
-       int l=0 ,r=0;
-       while(l<n && r<m){
-        if(nums1[l]<=nums2[r]){
-            ans.push_back(nums1[l]);
-            l++;
-        }else{
-            ans.push_back(nums2[r]);
-            r++;
+       int l=0;
+       while(l<n && n!=0&&m!=0){
+        if(nums2[0]<nums1[l]){
+            swap(nums2[0],nums1[l]);
         }
-       }
-       while(l<n){
-        ans.push_back(nums1[l]);
+        sort(nums2.begin(),nums2.end());
         l++;
        }
-       while(r<m){
-        ans.push_back(nums2[r]);
-        r++;
-       }
-       int s=m+n;
-       if(s%2==1){
-        return ans[s/2];
-       }else{
-        
-       return double(ans[s/2]+ans[(s/2)-1])/2;
+       int s=(m+n)/2;
+       if(s<n){
+        if((n+m)%2==1){
+            return nums1[s];
 
+        }else{
+            return double(nums1[s-1]+nums1[s])/2;
+        }
+       }else{
+        if((n+m)%2==1){
+            return nums2[s-n];
+        }else{
+            if(s==n){
+                return double(nums1[n-1]+nums2[s-n])/2;
+            }else{
+                return double(nums2[s-n-1]+nums2[s-n])/2;
+            }
+        }
        }
+     
 
         
     }
